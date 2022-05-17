8fdd3742fd0beb2b939e6fc81b311227db5eec7f
Mariam-Madbouli <mariam.madbouli@gmail.com>
2022-05-17 09:10:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const quotes = [
+  "you will look back and",
+  "feel proud of yourself.",
+  "Someday,",
+  "when things are okay",
+]
+
 function App() {
+  const [counter, setCounter] = useState(2)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onClick={(event) => setCounter(counter + 1)}  
+      className="App"
+    >
+      {quotes[counter % quotes.length]}
     </div>
   );
 }
